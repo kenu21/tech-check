8c8ac457d3729608ad500fbd064d3543663c56a1
iurii <kenu21@gmail.com>
2024-05-27 16:30:55 +0200

    set minimum to start spring Application

---
+++ src/main/java/org/keniu/TechCheckApplication.java
@@ -0,0 +1,11 @@
+package org.keniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechCheckApplication.class, args);
+    }
+}
