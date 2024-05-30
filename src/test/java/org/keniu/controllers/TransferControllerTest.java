74180f34aeb2f7adc9f7adb5a5fd8d5bba1bb634
iurii <kenu21@gmail.com>
2024-05-30 14:09:53 +0200

    refactored api test

--- src/test/java/org/keniu/controllers/TransferControllerTest.java
+++ src/test/java/org/keniu/controllers/TransferControllerTest.java
@@ -1,5 +1,6 @@
 package org.keniu.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.keniu.services.UserService;
@@ -9,13 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,17 +35,9 @@ class TransferControllerTest {
     void testTransferMoneySuccess() throws Exception {
         Mockito.doNothing().when(userService).transferMoney(anyString(), anyString(), any(BigDecimal.class));
 
-        Map<String, String> payload = new HashMap<>();
-        payload.put("fromUser", "user1");
-        payload.put("toUser", "user2");
-        payload.put("amount", "100.00");
+        Map<String, String> payload = createPayload();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonPayload = objectMapper.writeValueAsString(payload);
-
-        mockMvc.perform(post("/transfer")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonPayload))
+        mockMvc.perform(sendPostRequest(payload))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Transfer successful!"));
     }
@@ -53,18 +47,27 @@ class TransferControllerTest {
         Mockito.doThrow(new RuntimeException("Not enough balance to transfer"))
                 .when(userService).transferMoney(anyString(), anyString(), any(BigDecimal.class));
 
+        Map<String, String> payload = createPayload();
+
+        mockMvc.perform(sendPostRequest(payload))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Not enough balance to transfer"));
+    }
+
+    private Map<String, String> createPayload() {
         Map<String, String> payload = new HashMap<>();
         payload.put("fromUser", "user1");
         payload.put("toUser", "user2");
         payload.put("amount", "100.00");
+        return payload;
+    }
 
+    private MockHttpServletRequestBuilder sendPostRequest(Map<String, String> payload)
+            throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonPayload = objectMapper.writeValueAsString(payload);
-
-        mockMvc.perform(post("/transfer")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonPayload))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Not enough balance to transfer"));
+        return post("/transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonPayload);
     }
 }
